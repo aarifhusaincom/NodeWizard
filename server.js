062c372c5e45d38a7c8a248050a78247579bcf4e
Aarif Husain <mail@aarifhusain.com>
2023-06-04 10:25:51 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+var mysql = require('mysql');
+
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root"
+});
+
+con.connect( (err) =>  {
+  if (err)  throw err ;{
+      console.log("Connected!");
+  } 
+
+});
+
+app.listen(3000)
+// app.listen(3000)
+// from branch1
